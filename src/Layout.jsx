1dba77d9cf4204cc210d21fcf544528575576ffb
paramsavla06 <paramsavla06@gmail.com>
2026-04-10 00:42:55 +0530

    zoom scale fix needed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
-export default function Layout() {
-	return(
-		<>
-		<Header />
-		<Outlet />
-		<Footer />
-		</>
-	)
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
 }
+
+export default Layout
