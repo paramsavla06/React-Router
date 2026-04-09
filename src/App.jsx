a61f433184f80287e578c4a572221cc61e779775
paramsavla06 <paramsavla06@gmail.com>
2026-04-10 00:30:37 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
+import Home from './components/Home.jsx'
+import Footer from './components/Footer.jsx'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +12,8 @@ function App() {
   return (
     <>
       <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
